792499d50be4ebcc7f7511eb13693c6c72864abf
Saraswati Saud <saraswati.saud@gmail.com>
2017-12-08 12:43:21 +0545

    Added single line arrow function to refactor code

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -3,13 +3,13 @@ import React, { Component } from 'react';
 class SearchBar extends Component {
   render() {
     return(
-      <input onClick={this.onInputChange} />
+      <input onChange={event => console.log(event.target.value)} />
     );
   }
 
-  onInputChange(event) {
-    console.log(event.target.value);
-  }
+  // onInputChange(event) {
+  //   console.log(event.target.value);
+  // }
 }
 
 export default SearchBar;
