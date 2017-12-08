4abe97f41b98eeda77694109d3266c40244304c7
Saraswati Saud <saraswati.saud@gmail.com>
2017-12-08 13:24:05 +0545

    Fetching data from youtube-api-search

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+// use this to fetch data from youtube
+import YTSearch from 'youtube-api-search';
+
 import SearchBar from './components/search_bar.js';
 
 const API_KEY = 'AIzaSyB_zm0K9pV_-G0SL2MO6EMWwrmf2pEs-TM';
+
+YTSearch({key: API_KEY, term: 'surfboards'}, function(data) {
+  console.log(data);
+});
 
 // create a new component. This component should produce some HTML
 const App = () => {
