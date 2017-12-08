f02072ce641e03049a1c3323ad40ad55f2a4f7cd
Saraswati Saud <saraswati.saud@gmail.com>
2017-12-08 12:40:09 +0545

    Created user event handler

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const SearchBar = () => {
-  return(
-    <input />
-  ); 
+class SearchBar extends Component {
+  render() {
+    return(
+      <input onClick={this.onInputChange} />
+    );
+  }
+
+  onInputChange(event) {
+    console.log(event.target.value);
+  }
 }
 
 export default SearchBar;
